68c7254aa342814a0ad503c79116e4c39742dd9a
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-16 01:05:36 +0330

    Eliminate dialog

--- components/tasksList/TasksList.jsx
+++ components/tasksList/TasksList.jsx
@@ -14,11 +14,12 @@ import { auth, db } from "@/config/firebase";
 
 import { Backdrop, Checkbox, CircularProgress, Grid } from "@mui/material";
 
+import { ReactSortable } from "react-sortablejs";
+
 import { Msg, logStatus, Dark } from "@/helper/Contexts";
 
 import styles from "./TasksList.module.css";
-import NewTaskDialog from "./NewTaskDialog";
-import { ReactSortable } from "react-sortablejs";
+// import NewTaskDialog from "./NewTaskDialog";
 
 export default function TasksList() {
   const route = useRouter();
@@ -156,11 +157,11 @@ export default function TasksList() {
     <div className={`container ${darkMode ? "darkShadow" : "lightShadow"}`}>
       <div className="header">
         <h1>Tasks List</h1>
-        <NewTaskDialog
+        {/* <NewTaskDialog
           max={maxOrder}
           getList={getList}
           setLoading={setLoading}
-        />
+        /> */}
         <div onClick={route.back} className="backBtn">
           Back
         </div>
