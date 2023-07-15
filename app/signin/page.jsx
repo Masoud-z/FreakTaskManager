12736be3334a025cf5167a72573ebc750979c7a9
Masoud <masoud.zeinalabedini@gmail.com>
2023-07-15 17:03:14 +0330

    Add sign in page

---
+++ app/signin/page.jsx
@@ -0,0 +1,7 @@
+import SignIn from "@/components/signIn/SignIn";
+
+function page() {
+  return <SignIn />;
+}
+
+export default page;
--- components/signIn/SignIn.jsx
+++ components/signIn/SignIn.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState, useContext, useEffect } from "react";
 import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { useForm } from "react-hook-form";
